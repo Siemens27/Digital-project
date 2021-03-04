95bab0e64c03d7ea0492d46e752c6039c08d77d3
Kairat <kypchakbaevk1@gmail.com>
2021-03-04 10:55:45 +0300

    all changed

--- js/contacts.js
+++ js/contacts.js
@@ -1,5 +1,6 @@
 const feedBack = document.querySelector('.contacts-info__btn');
-const modalForm = document.querySelector('.modal');
+const modal = document.querySelector('.modal');
+const modalForm = document.querySelector('.modal__form')
 const modalClose = document.querySelector('.modal__close');
 const modalBtn = document.querySelector('.modal__btn');
 const modalThanks = document.querySelector('.modal-thanks');
@@ -8,16 +9,16 @@ const modalThanksBtn = document.querySelector('.modal-thanks__btn');
 
 
 feedBack.addEventListener('click', function(e){
-    modalForm.style.display = 'flex';
+    modal.style.display = 'flex';
     modalBtn.addEventListener('click', function(e){
-        modalForm.style.display = 'none'
+        modal.style.display = 'none'
         modalThanks.style.display = 'flex'
         e.preventDefault();
     })
 });
 
 modalClose.addEventListener('click', function() {
-    modalForm.style.display = 'none';
+    modal.style.display = 'none';
 });
 
 modalThanksBtn.addEventListener('click', function() {
@@ -28,10 +29,14 @@ modalThanksClose.addEventListener('click', function() {
     modalThanks.style.display = 'none';
 });
 
+modalForm.addEventListener('submit', function (e) { 
+    console.log(modalForm.name.value)
+})
+
 window.addEventListener('keydown', function(event) {
     if (event.key === 'Escape') {
-        modalForm.style.display = 'none'
+        modal.style.display = 'none'
         modalThanks.style.display = 'none';
     }
 });
-console.log(feedBack, modalForm);
+console.log(feedBack, modal);
